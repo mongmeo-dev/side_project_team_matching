0fbd77e6861078c9c19bad8570276cd5ab9abef1
Hongdae Kim <mongmeo.dev@gmail.com>
2022-12-09 10:49:37 +0900

    Initial commit

---
+++ src/main/java/dev/mongmeo/side_project_team_matching/SideProjectTeamMatchingApplication.java
@@ -0,0 +1,13 @@
+package dev.mongmeo.side_project_team_matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideProjectTeamMatchingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SideProjectTeamMatchingApplication.class, args);
+  }
+
+}
