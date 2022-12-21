a8048844beec91034177bd52db4d6954b3449ad4
Hongdae Kim <mongmeo.dev@gmail.com>
2022-12-21 19:31:54 +0900

    이메일 인증을 제외한 회원가입 관련 서비스 로직 작성

---
+++ src/main/java/dev/mongmeo/side_project_team_matching/service/user/UserService.java
@@ -0,0 +1,55 @@
+package dev.mongmeo.side_project_team_matching.service.user;
+
+import dev.mongmeo.side_project_team_matching.domain.model.user.SignUpModel;
+import dev.mongmeo.side_project_team_matching.domain.port.out.repository.user.UserQueryRepository;
+import dev.mongmeo.side_project_team_matching.domain.port.out.repository.user.UserUpsertRepository;
+import dev.mongmeo.side_project_team_matching.domain.port.in.usecase.user.SignUpUseCase;
+import dev.mongmeo.side_project_team_matching.entity.user.UserEntity;
+import dev.mongmeo.side_project_team_matching.exception.ErrorCode;
+import dev.mongmeo.side_project_team_matching.exception.RequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements SignUpUseCase {
+
+  private final UserUpsertRepository userUpsertRepository;
+  private final UserQueryRepository userQueryRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  @Transactional
+  public long signUpByEmail(SignUpModel model) {
+    checkEmailAndNicknameIsAlreadyUsed(model);
+
+    model.updatePasswordToEncodedPassword(passwordEncoder.encode(model.getPassword()));
+
+    UserEntity savedUser = userUpsertRepository.save(UserEntity.joinByEmail(model));
+
+    // TODO : 이메일 인증
+
+    return savedUser.getId();
+  }
+
+  private void checkEmailAndNicknameIsAlreadyUsed(SignUpModel model) {
+    checkEmailAlreadyUse(model.getEmail());
+    checkNicknameAlreadyUser(model.getNickname());
+  }
+
+  private void checkEmailAlreadyUse(String email) {
+    boolean result = userQueryRepository.existsByEmail(email);
+    if (result) {
+      throw new RequestException(ErrorCode.DUPLICATED_EMAIL);
+    }
+  }
+
+  private void checkNicknameAlreadyUser(String nickname) {
+    boolean result = userQueryRepository.existsByNickname(nickname);
+    if (result) {
+      throw new RequestException(ErrorCode.DUPLICATED_NICKNAME);
+    }
+  }
+}
---
+++ src/test/java/dev/mongmeo/side_project_team_matching/service/user/UserServiceTest.java
@@ -0,0 +1,83 @@
+package dev.mongmeo.side_project_team_matching.service.user;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+import dev.mongmeo.side_project_team_matching.domain.model.user.SignUpModel;
+import dev.mongmeo.side_project_team_matching.domain.port.out.repository.user.UserQueryRepository;
+import dev.mongmeo.side_project_team_matching.domain.port.out.repository.user.UserUpsertRepository;
+import dev.mongmeo.side_project_team_matching.entity.user.UserEntity;
+import dev.mongmeo.side_project_team_matching.exception.RequestException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+  @Mock
+  private UserUpsertRepository userUpsertRepository;
+  @Mock
+  private UserQueryRepository userQueryRepository;
+  @Mock
+  private PasswordEncoder passwordEncoder;
+  @InjectMocks
+  private UserService userService;
+
+  @Test
+  @DisplayName("SignUpModel 객체를 넘기면 UserEntity 객체를 생성하는데, 비밀번호는 해싱되어 저장한다.")
+  void signUpByEmailTest() {
+    // given
+    String encodedPassword = "encodedPassword";
+    SignUpModel model = new SignUpModel("test@test.com", "password", "tester", "tester");
+    BDDMockito.given(passwordEncoder.encode(model.getPassword())).willReturn(encodedPassword);
+    BDDMockito.given(userQueryRepository.existsByEmail(model.getEmail())).willReturn(false);
+    BDDMockito.given(userQueryRepository.existsByNickname(model.getNickname())).willReturn(false);
+    UserEntity mockSavedEntity = Mockito.mock(UserEntity.class);
+    BDDMockito.given(userUpsertRepository.save(any())).willReturn(mockSavedEntity);
+    BDDMockito.given(mockSavedEntity.getId()).willReturn(1L);
+
+    // when
+    long actual = userService.signUpByEmail(model);
+
+    // then
+    Assertions.assertThat(actual).isEqualTo(1L);
+    Mockito.verify(userUpsertRepository, Mockito.times(1))
+        .save(argThat(entity -> entity.getEmail().equals(model.getEmail())
+            && entity.getPassword().equals(encodedPassword) && entity.getName().equals(model.getName())
+            && entity.getNickname().equals(model.getNickname())));
+  }
+
+  @Test
+  @DisplayName("이미 사용중인 이메일로 회원가입을 시도하면 RequestException이 발생한다.")
+  void signUpDuplicatedEmailTest() {
+    // given
+    String email = "test@test.com";
+    SignUpModel model = new SignUpModel(email, "password", "tester", "tester");
+    BDDMockito.given(userQueryRepository.existsByEmail(email)).willReturn(true);
+
+    // when
+    // then
+    Assertions.assertThatThrownBy(() -> userService.signUpByEmail(model)).isInstanceOf(RequestException.class);
+  }
+
+  @Test
+  @DisplayName("이미 사용중인 닉네임으로 회원가입을 시도하면 RequestException이 발생한다.")
+  void SignUpDuplicatedNicknameTest() {
+    // given
+    String nickname = "tester";
+    SignUpModel model = new SignUpModel("test@test.com", "password", "tester", nickname);
+    BDDMockito.given(userQueryRepository.existsByNickname(nickname)).willReturn(true);
+
+    // when
+    // then
+    Assertions.assertThatThrownBy(() -> userService.signUpByEmail(model)).isInstanceOf(RequestException.class);
+  }
+}
